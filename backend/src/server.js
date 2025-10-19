56e8857de072b4965cd2e4ee862e1cc35795567e
Nelson2023-2024 <nelsonobuya19@gmail.com>
2025-10-19 18:52:43 +0300

    Added a health check endpoint for the API

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,6 +5,13 @@ import { connectToMongoDBWithRetry } from "./config/db.config.js";
 const app = express();
 
 app.use(express.json())
+
+app.get("/api/health", async (req,res) => {
+  console.log("Object1")
+  res.status(200).json({status: "ok",message:"Health"})
+})
+
+
 app.listen(ENV.PORT, () => {
   console.log(`Server running on http://localhost:${ENV.PORT}`);
   connectToMongoDBWithRetry()
