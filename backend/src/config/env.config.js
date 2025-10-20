47df7e0f1959e93ffea02aa97d0ca28f60c1e2ba
Nelson2023-2024 <nelsonobuya19@gmail.com>
2025-10-20 22:26:26 +0300

    ENV configuration file setup with ENVS

--- backend/src/config/env.config.js
+++ backend/src/config/env.config.js
@@ -1,7 +1,17 @@
-import {configDotenv} from "dotenv"
-configDotenv()
+import { configDotenv } from "dotenv";
+configDotenv();
 
 export const ENV = {
-    PORT: process.env.PORT,
-    MONGO_URI: process.env.MONGO_URI
-}
+  PORT: process.env.PORT,
+  MONGO_URI: process.env.MONGO_URI,
+  SESSION_SECRET: process.env.SESSION_SECRET,
+
+  //GOOGLE AUTH
+  GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+  GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+  GOOGLE_CALLBACK_URL: process.env.GOOGLE_CALLBACK_URL,
+
+  //REDIS
+  REDIS_URL:process.env.REDIS_URL,
+  REDIS_PORT:process.env.REDIS_PORT
+};
