568bf5a3aa6151edddf1eadaa2fc8f87e9b3f5f6
Nelson2023-2024 <nelsonobuya19@gmail.com>
2025-10-20 22:27:36 +0300

    Passport and redis middlware setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,15 +1,25 @@
 import express from "express";
+import passport from "passport";
 import { ENV } from "./config/env.config.js";
 import { connectToMongoDBWithRetry } from "./config/db.config.js";
+import { sessionMiddleware } from "./middleware/redis.middleware.js";
+import { authRoutes } from "./routes/auth.routes.js";
 
 const app = express();
 
 app.use(express.json())
+app.use(sessionMiddleware)
+
+//Intializes Passport for incoming requests, allowing authentication strategies to be applied.
+app.use(passport.initialize())
+app.use(passport.session())
 
 app.get("/api/health", async (req,res) => {
   console.log("Object1")
   res.status(200).json({status: "ok",message:"Health"})
 })
+
+app.use('/api/auth', authRoutes)
 
 
 app.listen(ENV.PORT, () => {
