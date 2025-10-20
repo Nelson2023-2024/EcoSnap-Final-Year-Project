808ce0ed86394d63ef5e18e223ce0860fba4c724
Nelson2023-2024 <nelsonobuya19@gmail.com>
2025-10-20 22:18:23 +0300

    Configured the users models

---
+++ backend/src/models/User.model.js
@@ -0,0 +1,62 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      trim: true,
+    },
+    lastName: {
+      type: String,
+      trim: true,
+    },
+    fullName: {
+      type: String,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    username: {
+      type: String,
+      required: [true, "Username is required"],
+      unique: true,
+      trim: true,
+    },
+    phoneNumber: {
+      type: String,
+      trim: true,
+    },
+    password: {
+      type: String,
+      trim: true,
+    },
+    profileImage: {
+      type: String, // URL to profile picture
+      default: "https://cdn-icons-png.flaticon.com/512/149/149071.png",
+      trim: true,
+    },
+    points: {
+      type: Number,
+      trim: true,
+      default: 0
+    },
+    //Used to search for the user safely
+    googleID: {
+      type: String,
+      unique: true,
+      trim: true,
+    },
+    authProvider: {
+      type: String,
+      trim: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export const User = mongoose.model('User', userSchema)
