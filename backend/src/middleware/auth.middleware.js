9acb9aacca6cbf1c942ef7fc8bd438d51d892744
Nelson2023-2024 <nelsonobuya19@gmail.com>
2025-11-02 16:49:33 +0300

    Added the adminProtect route middlware function

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -2,3 +2,14 @@ export const isAuthenticated = (req, res, next) => {
   if (req.isAuthenticated()) return next();
   res.status(401).json({ message: "Not authenticated" });
 };
+
+export const isAdmin = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    if (req.user.role === "admin") {
+      return next(); // If the user is an admin, allow access
+    } else {
+      return res.status(403).json({ message: "Access denied. Admins only." }); // If not an admin
+    }
+  }
+  res.status(401).json({ message: "Not authenticated" }); // If not authenticated
+};
