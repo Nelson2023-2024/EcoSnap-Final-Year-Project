996a3f18770e0790e03941e4f90c275fc96512f3
Nelson2023-2024 <nelsonobuya19@gmail.com>
2025-11-14 17:40:09 +0300

    Modified the /google/callback for better redirection based on user roles

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -3,6 +3,7 @@ import asyncHandler from "express-async-handler";
 import passport from "passport";
 import "../passport/google.auth.js";
 import { isAuthenticated } from "../middleware/auth.middleware.js";
+import { ENV } from "../config/env.config.js";
 
 const router = Router();
 
@@ -10,11 +11,19 @@ router.get("/google", passport.authenticate("google"));
 
 router.get(
   "/google/callback",
-  passport.authenticate("google"),
+  passport.authenticate("google", {
+    failureRedirect: `${ENV.FRONTEND_URL}/login`,
+  }),
   asyncHandler(async (req, res) => {
     console.log(req.session);
-    console.log(req.user);
-    res.sendStatus(200);
+    console.log("Authenticated user:", req.user);
+
+    const redirectURL =
+      req.user.role === "admin"
+        ? `${ENV.FRONTEND_URL}/admin`
+        : `${ENV.FRONTEND_URL}/user-dashboard`;
+    // Redirect to frontend after successful login
+    res.redirect(redirectURL);
   })
 );
 
@@ -26,11 +35,14 @@ router.get("/me", isAuthenticated, (req, res) => {
 
 router.get("/dev-login", async (req, res) => {
   const user = {
-    id: "69067633fbec6d2fa3fbb774",
+    id: "6913a19b1b5dab9aaed2a8a3",
     email: "nelsonobuya18@gmail.com",
   };
   req.login(user, (err) => {
-    if (err) return res.status(500).json({ message: "Login failed" , error: err.message});
+    if (err)
+      return res
+        .status(500)
+        .json({ message: "Login failed", error: err.message });
     res.json({ message: "Dev login successful", user });
   });
 });
